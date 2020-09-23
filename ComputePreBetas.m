f596eedbc8214c885ca3f97417077fa213e2a45c
SaiChrisZHANG <saizhang.econ@gmail.com>
2020-09-24 00:31:46 +0100

    commit

---
+++ ComputePreBetas.m
@@ -0,0 +1,126 @@
+%This function is called by mainToRunFamaMacBeth
+function [d,Mkt] = ComputePreBetas(ReturnSeries,LagAssetId,d,Mkt)
+
+    %% read the returns data i.e., compute excess returns
+    d = getFirmSpecificData(ReturnSeries,d);
+    
+    %compute excess market returns
+    if isempty(Mkt)
+        Mkt = getMarketData(d,LagAssetId);
+    end
+    
+    Mkt(:,3) = NaN;
+    %create lagged excess market return
+    Mkt(2:end,3) = Mkt(1:end-1,2);
+    disp(nanmean(Mkt(:,2:3)))
+    
+    %compute firm-specific betas and assign to next year
+    d = getFirmBetas(d,Mkt);
+    
+    function d = getFirmSpecificData(ReturnSeries,d)
+    d.Name = eval(strcat('d.',ReturnSeries));
+    d.NameExcess = d.Name - d.rfFFWebsite;
+    
+    
+    function Mkt = getMarketData(d,LagAssetId)
+    
+    Dates = unique(d.yyyymm);
+    Dates(isnan(Dates))  = [];
+    Dates(Dates<197107) = [];
+    
+    for j = 1:length(Dates)
+        
+        loc = find(d.yyyymm == Dates(j));
+        
+        if LagAssetId == 1
+            y = [d.NameExcess(loc),d.AssetValueLag(loc)];
+        else
+            y = [d.NameExcess(loc),d.meLag(loc)];
+        end
+        
+        [ir,ic] = find(isnan(y) == 1);
+        y(ir,:) = [];
+        
+    
+        if size(y,1) > 10
+            Mkt(j,:) = [Dates(j), sum(y(:,1).*y(:,2)/sum(y(:,2)))];
+        else
+            Mkt(j,:) = [Dates(j), NaN];
+        end
+    
+    end
+    
+    
+    function d = getFirmBetas(d,Mkt)
+    
+    %%run the capm model with lagged market to eestimate the betas using past five year of data
+    Dates = unique(d.yyyymm);
+    Dates(isnan(Dates))  = [];
+    Dates(Dates<197101) = [];
+    
+    Perm = unique(d.PERMNO);
+    Perm(isnan(Perm)) = [];
+    d1 = [];
+    disp(size(Perm))
+    
+    %running the estimation firm-by-firm
+    parfor k = 1:length(Perm)
+        
+        if mod(k,100) == 0
+            disp(k);
+        end
+        loc = d.PERMNO == Perm(k);
+        temp = d(loc,:);
+        
+        %for each firm compute the betas
+        temp = computeFirmBetas(temp,Mkt);
+        d1 = [d1;temp];
+        
+    end
+    
+    d = d1; clear d1;
+    
+    function temp = computeFirmBetas(temp,Mkt)
+    temp.FirmBetas = NaN*temp.yyyymm;
+    temp.JuneFirmBetas = temp.FirmBetas;
+    for j = 1971:2012
+        
+        %identify June, as well as identify past five year 
+        St = (j-5)*1e2 + 6;
+        En = j*1e2 + 6;
+        loc = find(temp.yyyymm > St & temp.yyyymm <= En);
+        y = [temp.yyyymm(loc), temp.NameExcess(loc)];
+        [ir,~] = find(isnan(y) == 1);
+        y(ir,:) = [];
+        
+        
+        %perform the capm regression if at least 24 monthly data points available
+        if size(y,1) >= 24
+            [~,ia,ib] = intersect(y(:,1),Mkt(:,1)); % return the index
+            tx = [ones(size(Mkt(ib,1))), Mkt(ib,2:3)];
+            [ir,~] = find(isnan(tx) == 1);
+            tx(ir,:) = [];
+            R = rank(tx);
+            if R == size(tx,2) % no colinear
+                y = [y(ia,2),Mkt(ib,2:3)];
+                tBetas = regress(y(:,1),[ones(size(y(:,1))),y(:,2:3)]);
+                tBetas = tBetas(2) + tBetas(3);
+            else
+                disp(R)
+                disp(Mkt(ib,2:3))
+                tBetas = NaN;
+            end
+        else        
+            tBetas = NaN;
+        end
+        
+        %identify next 1 year, assign the june beta to next one year
+        Enp1 = (j+1)*1e2 + 6;
+        loc = temp.yyyymm > En & temp.yyyymm <= Enp1;
+        temp.FirmBetas(loc) = tBetas;
+        
+        %also hold june beta in a separate column
+        loc = temp.yyyymm == En;
+        temp.JuneFirmBetas(loc) = tBetas;
+        
+    end
